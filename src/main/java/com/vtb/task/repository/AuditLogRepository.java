a62c705868c9c414d71e07e982e0f527d4b9d05f
MKHlebnikov <mikha3008@yandex.ru>
2023-03-14 09:59:44 +0300

    Deleted @Repository annotation in AuditLogRepository and TaskRepository

--- src/main/java/com/vtb/task/repository/AuditLogRepository.java
+++ src/main/java/com/vtb/task/repository/AuditLogRepository.java
@@ -4,6 +4,5 @@ import com.vtb.task.entity.AuditLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
 }
